25d140a5d1117dd3c12d8e668d7ea068b0843a66
Lisa Cornu <lisa.cornu@grenoble-inp.org>
2025-01-08 12:55:00 +0100

    enlevé bool du print

--- src/main/java/fr/ensimag/deca/tree/AbstractPrint.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractPrint.java
@@ -42,7 +42,7 @@ public abstract class AbstractPrint extends AbstractInst {
                 
                 for (AbstractExpr arg : getArguments().getList()){
                     Type argType = arg.verifyExpr(compiler, localEnv, currentClass);
-                    if (!(argType.isString() || argType.isFloat() || argType.isInt() || argType.isBoolean())){
+                    if (!(argType.isString() || argType.isFloat() || argType.isInt() )){
                         throw new ContextualError("Type is not String or Int or Float or Boolean", getLocation());
                     }
                 }
