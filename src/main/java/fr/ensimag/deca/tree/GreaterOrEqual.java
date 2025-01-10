d7749cf06bd37fad3ef891dcd0a28ca0b86849ed
MRAD <abdelmlek.mrad@grenoble-inp.org>
2025-01-10 12:12:17 +0100

    cmp finis

--- src/main/java/fr/ensimag/deca/tree/GreaterOrEqual.java
+++ src/main/java/fr/ensimag/deca/tree/GreaterOrEqual.java
@@ -4,6 +4,12 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.instructions.BLE;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
+import fr.ensimag.ima.pseudocode.Label;
 
 /**
  * Operator "x >= y"
@@ -16,15 +22,29 @@ public class GreaterOrEqual extends AbstractOpIneq {
     public GreaterOrEqual(AbstractExpr leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
     }
-
+     private static int i=0;
 
     @Override
     protected String getOperatorName() {
         return ">=";
     }
 
-    @Override
+     @Override
     protected void codeGenBinaryExpr(DecacCompiler compiler, DVal op1, GPRegister op2) {
-        throw new UnsupportedOperationException("not yet implemented");
+            compiler.addInstruction(new CMP(op1, op2));
+            Label case1=new Label ("greater_or_equal"+i);
+            Label case2=new Label("not_greater_or_equal"+i);
+            i++;
+            compiler.addInstruction(new BLE(case1));    //si la comparaison est vrai(cad l'op est sup ) --> on va a case2
+            compiler.addInstruction(new LOAD(new ImmediateInteger(0),op2));
+            compiler.addInstruction(new BRA(case2));
+            compiler.addLabel(case1);
+            compiler.addInstruction(new LOAD(new ImmediateInteger(1),op2));
+            compiler.addLabel(case2);
+            //compiler.addInstruction(new PUSH(Register.getR(2)));
+
+
+
+
     }
 }
--- src/main/java/fr/ensimag/deca/tree/LowerOrEqual.java
+++ src/main/java/fr/ensimag/deca/tree/LowerOrEqual.java
@@ -5,7 +5,13 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.Register;
-import fr.ensimag.ima.pseudocode.instructions.POP;
+import fr.ensimag.ima.pseudocode.instructions.BGE;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.ImmediateInteger;
+import fr.ensimag.ima.pseudocode.Label;
+
 
 /**
  *
@@ -16,15 +22,29 @@ public class LowerOrEqual extends AbstractOpIneq {
     public LowerOrEqual(AbstractExpr leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
     }
-
+    private static int i =0;
 
     @Override
     protected String getOperatorName() {
         return "<=";
     }
 
-    @Override
+     @Override
     protected void codeGenBinaryExpr(DecacCompiler compiler, DVal op1, GPRegister op2) {
-        throw new UnsupportedOperationException("not yet implemented");
+            compiler.addInstruction(new CMP(op1, op2));
+            Label case1=new Label ("lower_or_equal"+i);
+            Label case2=new Label("not_lower_or_equal"+i);
+            i++;
+            compiler.addInstruction(new BGE(case1));    //si la comparaison est vrai(cad l'op est sup ) --> on va a case2
+            compiler.addInstruction(new LOAD(new ImmediateInteger(0),op2));
+            compiler.addInstruction(new BRA(case2));
+            compiler.addLabel(case1);
+            compiler.addInstruction(new LOAD(new ImmediateInteger(1),op2));
+            compiler.addLabel(case2);
+            //compiler.addInstruction(new PUSH(Register.getR(2)));
+
+
+
+
     }
 }
