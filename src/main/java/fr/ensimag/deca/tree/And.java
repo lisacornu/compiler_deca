4f1bbeb159edac425dfdb881b813188f98598b52
Lisa Cornu <lisa.cornu@grenoble-inp.org>
2025-01-11 17:05:43 +0100

    AND et OR ok

--- src/main/java/fr/ensimag/deca/tree/And.java
+++ src/main/java/fr/ensimag/deca/tree/And.java
@@ -4,6 +4,11 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
 
 /**
  *
@@ -21,8 +26,30 @@ public class And extends AbstractOpBool {
         return "&&";
     }
 
+    // permet de discerner les labels entre les différents usages de AND au cours du programme
+    static int and_cpt = 0;
+
     @Override
     protected void codeGenBinaryExpr(DecacCompiler compiler, DVal op1, GPRegister op2) {
-        throw new UnsupportedOperationException("not yet implemented");
+        compiler.addInstruction(new LOAD(op1, GPRegister.R0));
+
+        // si op1 est nulle (=false), AND est faux
+        compiler.addInstruction(new CMP(0, GPRegister.R0));
+        compiler.addInstruction(new BEQ(new Label("and_false_case" + and_cpt)));
+
+        // si op2 est nulle, AND est faux
+        compiler.addInstruction(new CMP(0, op2));
+        compiler.addInstruction(new BEQ(new Label("and_false_case" + and_cpt)));
+
+        // les deux opérandes sont non nulles => AND est vrai
+        // étant donné que le résultat est stocké dans op2, il n'y a rien à faire dans ce cas
+        compiler.addInstruction(new BRA(new Label("and_end_case" + and_cpt)));
+
+        // sinon, on met op2 à faux, l'opération donne le résultat "faux"
+        compiler.addLabel(new Label("and_false_case" + and_cpt));
+        compiler.addInstruction(new LOAD(0, op2));
+
+        compiler.addLabel(new Label("and_end_case" + and_cpt));
+        and_cpt++;
     }
 }
