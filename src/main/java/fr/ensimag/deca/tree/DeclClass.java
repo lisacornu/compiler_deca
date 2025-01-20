8c162f42182a0547996581c03bfe8952583e6008
Eliot Clerc <eliot.clerc@grenoble-inp.org>
2025-01-20 11:42:52 +0100

    fix -p

--- src/main/java/fr/ensimag/deca/tree/DeclClass.java
+++ src/main/java/fr/ensimag/deca/tree/DeclClass.java
@@ -42,9 +42,9 @@ public class DeclClass extends AbstractDeclClass {
     @Override
     public void decompile(IndentPrintStream s) {
         if (parentClass!=null || parentClass.getName().getName()!="Object"){
-            s.println("class"+ className.getName().getName() + "extends " + parentClass.getName().getName());
+            s.println("class "+ className.getName().getName() + "extends " + parentClass.getName().getName());
         } else {
-            s.println("class  " + className.getName().getName());
+            s.println("class " + className.getName().getName());
         }
         s.println("{");
         s.indent();
