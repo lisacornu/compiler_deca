429575e4aa3076bb98eed10ef86faa5392a7533c
Fabien Galzi <fabien.galzi@grenoble-inp.org>
2025-01-20 13:06:52 +0100

    fix opcmp

--- src/main/java/fr/ensimag/deca/tree/AbstractOpCmp.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpCmp.java
@@ -25,7 +25,7 @@ public abstract class AbstractOpCmp extends AbstractBinaryExpr {
         // TODO : Probablement problèmes de int et float à traiter
         Type lefType = getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
         Type righType = getRightOperand().verifyExpr(compiler, localEnv, currentClass);
-        if ((lefType.isInt() || lefType.isFloat()) && (righType.isInt() || righType.isFloat())){
+        if ((lefType.isInt() || lefType.isFloat()) && (righType.isInt() || righType.isFloat())||lefType.isNull()||righType.isNull()||(righType.sameType(lefType))){
             Type booleanType = new BooleanType(compiler.createSymbol("boolean"));
             this.setType(booleanType);
             return booleanType;
