43ab2d068c687b4832675b7016a64d01862f3ea2
Abdelmlek Mrad <abdelmlek.mrad@grenoble-inp.org>
2025-01-21 10:52:22 +0000

    Update Assign.java

--- src/main/java/fr/ensimag/deca/tree/Assign.java
+++ src/main/java/fr/ensimag/deca/tree/Assign.java
@@ -1,5 +1,6 @@
-package fr.ensimag.deca.tree;
 
+package fr.ensimag.deca.tree;
+import java.util.ArrayList;
 import fr.ensimag.deca.codegen.RegisterHandler;
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.DecacCompiler;
@@ -13,6 +14,7 @@ import fr.ensimag.ima.pseudocode.DVal;
 import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.*;
+import java.util.HashMap;
 
 /**
  * Assignment, i.e. lvalue = expr.
@@ -31,6 +33,61 @@ public class Assign extends AbstractBinaryExpr {
 
     public Assign(AbstractLValue leftOperand, AbstractExpr rightOperand) {
         super(leftOperand, rightOperand);
+    }
+     public Type verifyExpr_opti(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+        Type lefType = getLeftOperand().verifyExpr(compiler, localEnv, currentClass);   
+        AbstractExpr rightExpDefinition = getRightOperand().verifyRValue(compiler, localEnv, currentClass, lefType);
+        this.setType(lefType);
+        if (lefType.isFloat() && rightExpDefinition.getType().isInt()){
+            ConvFloat conversionFloat = new ConvFloat(rightExpDefinition);
+            conversionFloat.verifyExpr(compiler, localEnv, currentClass);
+            setRightOperand(conversionFloat);
+        }else{
+            setRightOperand(rightExpDefinition);
+        }
+        if(getRightOperand() instanceof Identifier){
+            ((Identifier)getRightOperand()).usage(compiler);
+            String varNameStr_r = ((Identifier) getRightOperand()).getName().toString();
+            if(compiler.variablePropa.get(varNameStr_r)!=null){
+                String varNameStr = ((Identifier) getLeftOperand()).getName().toString();
+                ((Identifier)getLeftOperand()).literal=new IntLiteral(compiler.variablePropa.get(varNameStr_r));
+                compiler.variablePropa.put(varNameStr,compiler.variablePropa.get(varNameStr_r));
+            }
+        }
+        if (getLeftOperand() instanceof Identifier) {
+            // Récupérer le nom de la variable
+            String varNameStr = ((Identifier) getLeftOperand()).getName().toString();
+            int usageCount = compiler.variableUsageCount.getOrDefault(varNameStr, 0);
+            if (compiler.variableUsageCountdyna.containsKey(varNameStr)) {
+                ArrayList<Integer> dynamicInfo = compiler.variableUsageCountdyna.get(varNameStr);
+                dynamicInfo.add(0);
+                dynamicInfo.set(dynamicInfo.size() - 1, usageCount > 0 ? 1 : 0);
+           //     compiler.variableUsageCountdyna.put(varNameStr, dynamicInfo);
+            }
+
+            // Mettre à jour l'indice pour la variable
+            int previousIndice = compiler.variableLast.getOrDefault(varNameStr, 0);
+            int newIndice = previousIndice + 1;
+            ((Identifier) getLeftOperand()).indice = newIndice;
+            compiler.variableLast.put(varNameStr, newIndice);
+            compiler.variableUsageCount.put(varNameStr, 0);
+        }
+        if(getRightOperand() instanceof IntLiteral){
+            String varNameStr = ((Identifier) getLeftOperand()).getName().toString();
+            ((Identifier)getLeftOperand()).literal=new IntLiteral(((IntLiteral)(getRightOperand())).getValue());
+            compiler.variablePropa.put(varNameStr,((IntLiteral)(getRightOperand())).getValue());
+        }
+        else{
+            String varNameStr = ((Identifier) getLeftOperand()).getName().toString();
+            compiler.variablePropa.put(varNameStr,null);
+        }
+        
+        // Incrémenter l'usage de la variable dans la table de hachage
+       // String varNameStr = getName().toString();
+       // compiler.variableUsageCount.put(varNameStr, compiler.variableUsageCount.get(varNameStr) + 1);  // Incrémenter l'usage
+        return lefType;
+        
     }
 
     @Override
@@ -55,40 +112,50 @@ public class Assign extends AbstractBinaryExpr {
         return "=";
     }
 
-    @Override
+  @Override
     protected DVal codeGenExpr(DecacCompiler compiler) {
+           // Vérifier l'utilisation de la variable dans la table de hachage
+        String varNameStr = ((Identifier)getLeftOperand()).getName().getName();
+        if (compiler.variableUsageCountdyna.containsKey(varNameStr)) {
+            ArrayList<Integer> dynamicInfo = compiler.variableUsageCountdyna.get(varNameStr);
 
-        // Generation du code de la branch de droite
-        DVal rightOperandResult = getRightOperand().codeGenExpr(compiler);
-
-        //On recupere l'adresse de la Lvalue
-        DAddr varAddress;
-        if (getLeftOperand() instanceof Selection) {
-            varAddress = ((Selection) getLeftOperand()).codeGenExprAddr(compiler, Register.R0);
-        } else {
-            Identifier leftOperandIdent =  (Identifier)getLeftOperand();
-            if (leftOperandIdent.getDefinition().isField()) {
-                varAddress = ((Identifier)getLeftOperand()).codeGenExprAddr(compiler, Register.R0);
-            } else {
-                varAddress = ((Identifier)getLeftOperand()).getExpDefinition().getOperand();
+            // Vérifier le premier élément du tableau
+            if (dynamicInfo.get(((Identifier)getLeftOperand()).indice) == 0  && compiler.opti==1) {
+                compiler.addComment("Variable " + varNameStr + ((Identifier)getLeftOperand()).indice +" n'a pas besoin d'etre assigné");
+                return null; // Ne pas générer la déclaration de la variable
             }
         }
-
-
-        // On recupere rightOperandResult dans un registre
+        // Generation du codes des branches
+        DVal leftOperandResult = getLeftOperand().codeGenExpr(compiler);
+        DVal rightOperandResult;
+        if(getRightOperand() instanceof Identifier){
+                
+              if(((Identifier)getLeftOperand()).literal!=null){
+                compiler.addComment("jspquoidire");
+                rightOperandResult =(( Identifier)getLeftOperand()).literal.codeGenExpr(compiler);
+              }
+              else{
+                rightOperandResult = getRightOperand().codeGenExpr(compiler);
+              }
+        }
+        else{
+            rightOperandResult = getRightOperand().codeGenExpr(compiler);
+        }
+        // Selection des bonnes adresses en fonction de leur emplacement mémoire
         GPRegister op2 =  RegisterHandler.popIntoRegister(compiler, rightOperandResult, Register.R1);
+        DVal op1 = RegisterHandler.popIntoDVal(compiler, leftOperandResult, Register.R0);
 
         // Generation du code de l'expression (résultat enregistré dans op1)
-        codeGenBinaryExpr(compiler, varAddress, op2);
+        codeGenBinaryExpr(compiler, op1, op2);
+        compiler.registerHandler.SetFree(op2);
 
         //Renvoi du résultat (op1 est ne peut pas être un registre temporaire)
-        return op2;
+        return op1;
     }
 
 
     @Override
     protected void codeGenBinaryExpr(DecacCompiler compiler, DVal op1, GPRegister op2) {
-        compiler.addInstruction(new STORE(op2,(DAddr)op1));
+        DAddr varAddress = ((AbstractIdentifier)getLeftOperand()).getExpDefinition().getOperand();
+        compiler.addInstruction(new STORE(op2,varAddress));
     }
-
-}
