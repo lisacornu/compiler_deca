e1552eaaeef6d3b83443f8776a4a82946e3bb107
Abdelmlek Mrad <abdelmlek.mrad@grenoble-inp.org>
2025-01-21 10:43:03 +0000

    Update AbstractOpArith.java

--- src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
@@ -54,3 +54,45 @@ public abstract class AbstractOpArith extends AbstractBinaryExpr {
         }
     }
 }
+
+
+ @Override
+    public Type verifyExpr_opti(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+        // TODO : vérifier si int et float, return type int ou float selon si opération sur des floats ou int
+        // Type opArith = new ExpDefinition(compiler.createSymbol(getOperatorName())); 
+        Type lefType = getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
+        Type righType = getRightOperand().verifyExpr(compiler, localEnv, currentClass);
+        if(getRightOperand() instanceof Identifier){
+            ((Identifier)getRightOperand()).usage(compiler);
+        }
+        if(getLeftOperand() instanceof Identifier){
+            ((Identifier)getLeftOperand()).usage(compiler);
+        }
+        if ((lefType.isInt() || lefType.isFloat()) && (righType.isFloat()||righType.isInt())){
+            if (lefType.isFloat() && righType.isInt()){
+                ConvFloat FloatTypeExpr = new ConvFloat(getRightOperand());
+                FloatTypeExpr.setType(righType);
+                setRightOperand(FloatTypeExpr); 
+                this.setType(lefType);
+                return lefType;
+
+            }else if (righType.isFloat() && lefType.isInt()){
+                ConvFloat FloatTypeExpr = new ConvFloat(getLeftOperand());
+                FloatTypeExpr.setType(righType);
+                setLeftOperand(FloatTypeExpr); 
+                this.setType(righType);
+                return righType;
+
+            }else if (righType.isFloat()){
+                this.setType(righType);
+                return righType;
+            }
+            this.setType(righType);
+            return righType;
+        }else{
+            throw new ContextualError("Both are not float or int",getLocation());
+        }
+    }
+}
+
