ed80a83703ad5054262f37457234d22c56dd693d
Eliot Clerc <eliot.clerc@grenoble-inp.org>
2025-01-22 17:04:20 +0100

    push sauveur

--- src/main/java/fr/ensimag/deca/tree/Plus.java
+++ src/main/java/fr/ensimag/deca/tree/Plus.java
@@ -45,8 +45,8 @@ public class Plus extends AbstractOpArith {
             }
             else if(operand instanceof Identifier){
                 //récupère le nom de l'identificateur
-                String identfierName = ((Identifier) operand).getName().getName();
-                result += compiler.variablePropa.get(identfierName);
+                String identName = ((Identifier) operand).getName().getName();
+                result += compiler.variablePropa.get(identName);
             }
             else{
                 result += ((AbstractOpArith) operand).evalExprValue(compiler);
